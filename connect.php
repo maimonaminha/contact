f27430c95fe221f84e6ca89332c2b4001629a365
Maimona minha <116954037+maimonaminha@users.noreply.github.com>
2023-11-19 09:35:46 +0600

    connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,20 @@
 <?php
-    $fullname = $_POST['fullname']
-    $email = $_POST['email']
-    $message = $_POST['message']
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $conn = new mysqli('localhost', 'root', '', 'contact');
+    if($conn->connect_errno){
+        die('Connection Failed : '.$conn->connect_errno);
+    }
+    else{
+        $stmt = $conn->prepare("insert into contactinfo(fullname, email, message)
+        values(?, ?, ?)");
+        $stmt->bind_param("sss", $fullname, $email, $message);
+        $stmt->execute();
+        echo "Successfully sent!";
+        $stmt->close();
+        $conn->close();
+    }
 ?>
+
